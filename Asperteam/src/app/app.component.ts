cf4f8e2be4db85e35199cf42d10041ec559e1732
Wesley PIN <wpin@itop.local>
2019-06-06 17:11:58 +0200

    [Asperteam] Début de la page de connexion

--- Asperteam/src/app/app.component.ts
+++ Asperteam/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, transition, animate, trigger, state, style } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('activeDesactive', [
+      state('active', style({
+        opacity: 1
+      })),
+      state('desactive', style({
+        opacity: 0
+      })),
+      transition('active => desactive', [
+        animate('1s')
+      ]),
+      transition('desactive => active', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'app works!';
+  images = [true, false, false, false];
+  indexImage = 0;
+
+
+  constructor() {
+    setInterval(() => this.changeImage(), 5000);
+  }
+
+  changeImage() {
+    switch (this.indexImage) {
+      case 0:
+        this.images[this.indexImage] = false;
+        this.indexImage++;
+        this.images[this.indexImage] = true;
+        break;
+      case 1:
+        this.images[this.indexImage] = false;
+        this.indexImage++;
+        this.images[this.indexImage] = true;
+        break;
+      case 2:
+        this.images[this.indexImage] = false;
+        this.indexImage++;
+        this.images[this.indexImage] = true;
+        break;
+      case 3:
+        this.images[this.indexImage] = false;
+        this.indexImage = 0;
+        this.images[this.indexImage] = true;
+        break;
+    }
+  }
 }
