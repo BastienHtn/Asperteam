f2571dc2e4e69d875d6c73a80dec72fc4dd31484
Wesley PIN <wpin@itop.local>
2019-06-16 11:56:26 +0200

    [Asperteam] Changement de la structure avec les routes

--- Asperteam/src/app/accueil/accueil.component.ts
+++ Asperteam/src/app/accueil/accueil.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'accueil-root',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        display: 'inline-block',
+        // margin: '0px 3rem 0px 0px'
+      })),
+      state('close', style({
+        display: 'none',
+        // margin: '0px 0px 0px 0px'
+      })),
+      transition('open => close', [
+        animate('0s')
+      ]),
+      transition('close => open', [
+        animate('0s')
+      ]),
+    ]),
+  ],
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
 
+  public open: boolean;
 
-  constructor() {
+  constructor(private location: Location) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  openClose() {
+    if (this.open) {
+      this.open = false;
+    } else {
+      this.open = true;
+    }
   }
 }
